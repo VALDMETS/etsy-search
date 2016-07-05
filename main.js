7f984929481d0699ec4412dd2cd05d955f2f7d55
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-07-05 16:41:17 -0500

    multiple search capability

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
 var itemArray = [];
+var searchTerm = 'bowl';
 var searchSettings = {
   dataType: 'jsonp',
   type: 'GET',
-  url: 'https://api.etsy.com/v2/listings/active.js?api_key=gp1swaytrsiu2jnlf92eygze&keywords=guitar&includes=Images,Shop',
-  success: function (response) {
-    itemArray = response.results;
-    // console.log(itemArray[0].Images[0]);
-    console.log(itemArray);
-    itemArray.forEach(function(item, i){
-        // $('.maindisplay').append('<img src=' + item.Images[0].url_170x135 + '>');
-        var newElement = $('<a class="itemdisplay"></a>');
-        $('.maindisplay').append(newElement);
-        newElement.attr('href', item.url);
-        newElement.append('<img src=' + item.Images[0].url_570xN + '>');
-        if (item.title.length > 38) {
-          titleAbbrev = item.title.slice(0,37) + '...';
-        } else {
-          titleAbbrev = item.title;
-        }
-        newElement.append('<p class="title">' + titleAbbrev+ '</p>');
-        newElement.append('<p class="maker">' + item.Shop.shop_name + '</p>');
-        newElement.append('<p class="cost">$' + item.price + '</p>');
-    });
-    // $('a').addClass('itemdisplay');
-  }
+  url: 'https://api.etsy.com/v2/listings/active.js?api_key=gp1swaytrsiu2jnlf92eygze&keywords=' + searchTerm + '&includes=Images,Shop',
+  success: settingGet
 };
+function settingGet (response) {
+  $('.maindisplay').html('');
+  // document.querySelector('.maindisplay').innerHTML = '';
+  itemArray = response.results;
+  itemArray.forEach(function(item, i){
+      console.log('wow');
+      var newElement = $('<a class="itemdisplay"></a>');
+      $('.maindisplay').append(newElement);
+      newElement.attr('href', item.url);
+      newElement.append('<img src=' + item.Images[0].url_570xN + '>');
+      if (item.title.length > 38) {
+        titleAbbrev = item.title.slice(0,37) + '...';
+      } else {
+        titleAbbrev = item.title;
+      }
+      newElement.append('<p class="title">' + titleAbbrev+ '</p>');
+      newElement.append('<p class="maker">' + item.Shop.shop_name + '</p>');
+      newElement.append('<p class="cost">$' + item.price + '</p>');
+  });
+  $('.maindisplay').prepend('<h4>Top Results for "' + searchTerm + '"</h4>');
+}
 
 $.ajax(searchSettings);
+
+
+$('input').change(function(evt){
+  searchTerm = evt.target.value;
+  searchSettings.url='https://api.etsy.com/v2/listings/active.js?api_key=gp1swaytrsiu2jnlf92eygze&keywords=' + searchTerm + '&includes=Images,Shop';
+  $.ajax(searchSettings);
+});
